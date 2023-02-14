c125b5c47544ae79a74ae61e40151bb57fe1acbd
akpi816218 <816218@seq.org>
2023-02-14 13:45:22 -0800

    Update to TS

--- dist/DefaultPronouns.js
+++
@@ -1,8 +0,0 @@
-import { Pronoun } from './Pronoun.js';
-import { PronounCodes } from './PronounCodes.js';
-export const DefaultPronouns = {
-	theyThem: new Pronoun(PronounCodes.theyThem),
-	heHim: new Pronoun(PronounCodes.theyThem),
-	sheHer: new Pronoun(PronounCodes.sheHer),
-	other: new Pronoun(PronounCodes.other),
-};
--- dist/Util.js
+++
@@ -1,7 +0,0 @@
-import { PronounCodes } from './PronounCodes.js';
-export function isPronounValue(s) {
-	return s in PronounCodes || /CustomPronoun\:[A-Z][a-z]+\/[A-Z][a-z]+/.test(s);
-}
-export function isPronounObject(o) {
-	return 'prnnbjct' in o;
-}
